36697ade729043231309e6d418c45deca0d1b3e7
Linn Htet Oo <lhtetoo@gmail.com>
2019-07-22 22:27:36 +0800

    Load todo list from data file

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "Register modules for upcoming semester",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Complete effective online writing course",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Study Redux",
+        completed: false
+    }
+]
+
+export default todosData
