b4fbb4148f648858b75ffafaa615283152b3db60
Linn Htet Oo <lhtetoo@gmail.com>
2019-07-22 22:45:17 +0800

    Updates

---
+++ precache-manifest.2e18cdce340f4f2d58af7986f46b7b39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b831e9b2636696cb76863c0723d3118",
+    "url": "/to-do-list-in-react/index.html"
+  },
+  {
+    "revision": "5e3027028c0a0c861e8f",
+    "url": "/to-do-list-in-react/static/css/main.da82452f.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/to-do-list-in-react/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "5e3027028c0a0c861e8f",
+    "url": "/to-do-list-in-react/static/js/main.4ec92edd.chunk.js"
+  },
+  {
+    "revision": "45d5baf1d242e9c88d25",
+    "url": "/to-do-list-in-react/static/js/runtime~main.151da059.js"
+  }
+]);
